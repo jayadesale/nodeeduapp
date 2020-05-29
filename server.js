c24888c842e593aa6069a0b48809522d1011a4f5
Jaya <jdesale@gmail.com>
2020-05-29 08:40:04 -0700

    Revert "Update server.js"
    This reverts commit c96805c28cdfd2624f6e4d983e0154be1eed3764.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req,res){
-    res.write('<h1>Welcome welcome to Simpli Node App</h1>')
+    res.write('<h1>Welcome to Simpli Node App</h1>')
     res.end()
 })
 
